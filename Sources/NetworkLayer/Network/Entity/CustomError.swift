ada9d3f69a4565a0544fc203bdbdf0fd556a0a84
Emirhan <emirhan@vivoo.me>
2024-08-20 16:10:04 +0300

    improvements response parser

--- Sources/NetworkLayer/Network/Entity/CustomError.swift
+++ Sources/NetworkLayer/Network/Entity/CustomError.swift
@@ -3,10 +3,10 @@ import Foundation
 public struct CustomError: Error, Codable {
     public var statusCode: Int?
     public var error: String?
-    public var message: [String]?
+    public var message: String?
     public var errorCode: String?
     
-    init(statusCode: Int? = nil, error: String? = nil, message: [String]? = nil, errorCode: String? = nil) {
+    init(statusCode: Int? = nil, error: String? = nil, message: String? = nil, errorCode: String? = nil) {
         self.statusCode = statusCode
         self.error = error
         self.message = message
--- Sources/NetworkLayer/Network/Service/AppResponseParser.swift
+++ Sources/NetworkLayer/Network/Service/AppResponseParser.swift
@@ -6,13 +6,16 @@ public class AppResponseParser {
             switch httpResponse.httpCode {
             case 401:
                 throw GenericErrorType.notAuthorized
-            case 400...:
+            case 400...499:
                 let response = try JSONDecoder().decode(BaseResponse<T>.self, from: httpResponse.data)
                 throw GenericErrorType.errorResponse(response: response.error!)
+            case 500:
+                let response = try JSONDecoder().decode(CustomError.self, from: httpResponse.data)
+                throw GenericErrorType.unknown(description: response.message)
             default:
                 let response = try JSONDecoder().decode(BaseResponse<T>.self, from: httpResponse.data)
                 guard let data = response.data else {
-                    throw GenericErrorType.decodingError(description: response.error?.message?.joined())
+                    throw GenericErrorType.decodingError(description: response.error?.message)
                 }
                 return data
             }
